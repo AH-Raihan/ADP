93b765427eaed41d4b3bd819b01a6e571a523702
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-08-21 14:38:56 +0600

    charset

--- config.php
+++ config.php
@@ -1,1 +1,3 @@
-<?php  $conn=mysqli_connect("sql5.freemysqlhosting.net","sql5431972","EGtJCHd7bv","sql5431972"); ?>
+<?php  $conn=mysqli_connect("sql5.freemysqlhosting.net","sql5431972","EGtJCHd7bv","sql5431972"); 
+mysqli_set_charset($conn,"utf8");
+?>
