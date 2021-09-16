9ecae1c02bebe1563a5df19203883f4a952080a7
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-09-17 00:21:38 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,6 @@
 window.addEventListener('scroll',function(){
   var sitebranding=document.getElementsByClassName('sitebranding');
   sitebranding[0].classList.toggle('sticky', window.scrollY > 0);
+  var logo=document.getElementsByClassName('logo-area');
+  logo[0].classList.toggle('dnone', window.scrollY > 0);
 });
