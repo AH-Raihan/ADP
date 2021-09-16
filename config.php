0b76ac2319b789831afb00cb3daa0558369d350d
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-09-16 21:08:32 +0600

    mysql

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
-<?php  $conn=mysqli_connect("sql5.freemysqlhosting.net","sql5431972","EGtJCHd7bv","sql5431972"); 
+<?php  $conn=mysqli_connect("b2orz6phnrhmfroybnul-mysql.services.clever-cloud.com","b2orz6phnrhmfroybnul","uqtulb4fedbg33nv","cJQxsWc26vHMikgEIARy"); 
 mysqli_set_charset($conn,"utf8");
 ?>
