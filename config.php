bd640b604f94764430d0a17fbd35417315761522
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-09-16 21:33:18 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
-<?php  $conn=mysqli_connect("b2orz6phnrhmfroybnul-mysql.services.clever-cloud.com","b2orz6phnrhmfroybnul","uqtulb4fedbg33nv","cJQxsWc26vHMikgEIARy"); 
+<?php  $conn=mysqli_connect("b2orz6phnrhmfroybnul-mysql.services.clever-cloud.com","cJQxsWc26vHMikgEIARy","uqtulb4fedbg33nv","b2orz6phnrhmfroybnul"); 
 mysqli_set_charset($conn,"utf8");
 ?>
