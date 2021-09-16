d32983c77fce60ec52650856b17662cafb7533ef
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-09-17 01:53:28 +0600

    Update controller.php

--- controller.php
+++ controller.php
@@ -25,21 +25,21 @@ if(isset($code)) {
         
         $emailaddr=$pay_load["email"];
         $fullname=$pay_load["name"];
-        $authToken=md5(sha1($emailaddr.$fullname));
+        $authToken=md5(sha1($emailaddr.'raihan123'));
         
         $mathQuery="SELECT * FROM users WHERE email_addr='$emailaddr'";
         $runSelectQuery=mysqli_query($conn,$mathQuery);
         $checkCount=mysqli_num_rows($runSelectQuery);
     
     if ($checkCount===0) {
-        $insert="INSERT INTO users (usrauthToken,email_addr,full_name) VALUES('$authToken','$emailaddr','$fullname')";
+        $insert="INSERT INTO users(usrauthToken,email_addr,full_name) VALUES('$authToken','$emailaddr','$fullname')";
         $insertQuery=mysqli_query($conn,$insert);
 
         if ($insertQuery==true) {
             setcookie("PHPLGADP",$authToken,time()+86400*360);
             header("location:index.php");
         } else {
-            header("location:signin.php?".$authToken.$emailaddr.$fullname);
+            header("location:signin.php");
         }
     }else{
         setcookie("PHPLGADP",$authToken,time()+86400*360);
