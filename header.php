c43225cac3b04f4e1b3dbc80b506bf5af9260a07
AH-Raihan <web.ahraihan@gmail.com>
2021-09-18 11:22:10 +0600

    manifest added

--- header.php
+++ header.php
@@ -36,6 +36,7 @@ if (isset($_COOKIE["PHPLGADP"])) {
         <link href="css/responsive.min.css" rel="stylesheet" type="text/css">
         <link href="css/other.css" rel="stylesheet" type="text/css">
         <link href="css/products-details.css" rel="stylesheet" type="text/css">
+	<link rel="manifest" href="manifest.json">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" />
         <style>
