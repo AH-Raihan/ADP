2e6b8fc7d970004ab1217f68e2453fcd2e949c03
AH-Raihan <web.ahraihan@gmail.com>
2021-10-19 17:10:17 +0600

    admin

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -7,7 +7,7 @@
     header("location:login.php");
 }
 ?>
-<!DOCTYPE html>
+<!DOCTYPE html
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -79,7 +79,7 @@
         <div class="grid_12">
             <ul class="nav main">
                 <li class="ic-dashboard"><a href="index.php"><span>Dashboard</span></a> </li>
-                <li class="ic-form-style"><a href=""><span>User Profile</span></a></li>
+                <li class="ic-form-style"><a href="#"><span>User Profile</span></a></li>
 				<li class="ic-typography"><a href="changepassword.php"><span>Change Password</span></a></li>
 				<li class="ic-grid-tables"><a href="inbox.php"><span>Inbox</span></a></li>
                 <li class="ic-charts"><a href="/"><span>Visit Website</span></a></li>
