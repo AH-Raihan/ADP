8260858cd1b4615ad56ed737099dfe6e3f30a83c
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-10-27 10:35:02 +0600

    Update books.php

--- books.php
+++ books.php
@@ -27,6 +27,7 @@
                         <div class="book-details">
                             <h3 class="book-title productTitle"><?php echo $books["book_name"]; ?></h3>
                             <h4 class="book-writer"><?php echo $books["book_writer"]; ?></h4>
+                            <div class="prices">
                             <?php $salePrice = $books["book_sale_price"];
                             if ($salePrice > 0) { ?>
 
@@ -40,6 +41,7 @@
                                 <span class="book-present-price">TK. <span class="productPrice"><?php echo $books["book_sale_price"]; ?></span></span>
 
                             <?php  } ?>
+                            </div>
 
                         </div>
                         <a class="book-view" href="product-details.php?book_name=<?php echo $books['book_name']; ?>">View Details</a>
