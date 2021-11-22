8be4694a561d71edbb6d5b2bda814a5d469e894d
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-11-22 16:14:03 +0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,11 @@
-﻿<?php include 'inc/header.php'; ?>
+<?php include 'inc/header.php'; ?>
 
 <?php include 'inc/sidebar.php'; ?>
 <div class="grid_10">
   <div class="box round first grid">
     <h2> Dashbord</h2>
     <div class="block">
-      <div class="totalOrderDashboard" style="background: #4c4c4c;color: white;padding: 10px;font-size: 20px; font-weight: 700;display: inline-block;border-radius:8px;">
+      <div class="totalOrderDashboard" style="background: #4c4c4c;">
         Total Orders :
         <?php
         require_once("../config.php");
@@ -17,7 +17,7 @@
       </div>
 
 
-      <div class="totalOrderDashboard" style="background: #4c4c4c;color: white;padding: 10px;font-size: 20px; font-weight: 700;display: inline-block;border-radius:8px;">
+      <div class="totalOrderDashboard" style="background: #00a712;">
         Completed Orders :
         <?php
         $selectOrdersQuery = "SELECT * FROM orders WHERE order_status='Completed'";
@@ -26,7 +26,25 @@
         ?>
       </div>
 
-      <div class="totalOrderDashboard" style="background: #4c4c4c;color: white;padding: 10px;font-size: 20px; font-weight: 700;display: inline-block;border-radius:8px;">
+      <div class="totalOrderDashboard" style="background: #056a9f;">
+        Pending Orders :
+        <?php
+        $selectOrdersQuery = "SELECT * FROM orders WHERE order_status='Pending'";
+        $queryRun = mysqli_query($conn, $selectOrdersQuery);
+        echo mysqli_num_rows($queryRun);
+        ?>
+      </div>
+
+      <div class="totalOrderDashboard" style="background: #e9c106;">
+        Waiting Orders :
+        <?php
+        $selectOrdersQuery = "SELECT * FROM orders WHERE order_status='Waiting'";
+        $queryRun = mysqli_query($conn, $selectOrdersQuery);
+        echo mysqli_num_rows($queryRun);
+        ?>
+      </div>
+
+      <div class="totalOrderDashboard" style="background: #f95d5d;">
         Cancel Orders :
         <?php
         $selectOrdersQuery = "SELECT * FROM orders WHERE order_status='Cenceled'";
