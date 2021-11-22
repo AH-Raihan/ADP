0cf7065b7860a0922bdc4fe4a5fb69fb48963a17
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-11-22 21:39:44 +0600

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -59,7 +59,7 @@
 				</div>
 				<div class="floatleft middle">
 					<h1>Alor Dishari Publications</h1>
-					<p>www.alordisharipublications.com</p>
+					<!-- <p>www.alordisharipublications.com</p> -->
 				</div>
                 <div class="floatright">
                     <div class="floatleft">
@@ -80,7 +80,7 @@
         <div class="grid_12">
             <ul class="nav main">
                 <li class="ic-dashboard"><a href="index.php"><span>Dashboard</span></a> </li>
-                <li class="ic-form-style"><a href=""><span>User Profile</span></a></li>
+                <li class="ic-form-style"><!-- <a href=""><span>User Profile</span></a>--></li>
 				<li class="ic-typography"><a href="changepassword.php"><span>Change Password</span></a></li>
 				<li class="ic-grid-tables"><a href="inbox.php"><span>Inbox</span></a></li>
                 <li class="ic-charts"><a href="/"><span>Visit Website</span></a></li>
