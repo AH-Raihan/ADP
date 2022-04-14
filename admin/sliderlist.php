6e6f873f968338a8cf1e7e0cbf072fd62486dbe6
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-14 09:13:16 +0600

    Create sliderlist.php

---
+++ admin/sliderlist.php
@@ -0,0 +1,50 @@
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/sidebar.php'; ?>
+<div class="grid_10">
+  <div class="box round first grid">
+    <h2>Slider List</h2>
+    <div class="block">
+      <div class="bg-white text-dark content w-100 " style="overflow-x: auto;">
+        <table class="data display datatable " style="overflow-x: auto;">
+          <thead>
+            <tr>
+              <td>Image</td>
+              <td>Slider Title</td>
+              <td>Slider Url</td>
+<!--               <td>Edit</td> -->
+              <td>Delete</td>
+            </tr>
+          </thead>
+          <tbody>
+          <?php require_once("../config.php");
+          $ordersQ = "SELECT * FROM slider";
+          $orderQuery = mysqli_query($conn, $ordersQ);
+
+          while ($orders = mysqli_fetch_array($orderQuery)) { ?>
+            
+              <tr>
+              <td><img loading="lazy" width="80px" src="../images/<?php echo $orders["image"]; ?>" alt=""> </td>
+                <td><?php echo $orders["tilte"]; ?> </td>
+                <td><?php echo $orders["image"]; ?> </td>
+<!--                 <td><a onclick="return confirm('Are You Sure?')" href="sliderEdit.php?newsEditId=<?php echo $orders['id']; ?>">Edit</a></td> -->
+                <td><a onclick="return confirm('Are You Sure?')" href="addNewCore.php?sliderDeleteID=<?php echo $orders['id']; ?>">Delete</a></td>
+                
+              </tr>
+            
+          <?php } ?>
+          </tbody>
+
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    setupLeftMenu();
+    $('.datatable').dataTable();
+    setSidebarHeight();
+  });
+</script>
+<?php include 'inc/footer.php'; ?>
