cf256697c0734fa8b013d27aa54e55531c5424ea
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-14 20:03:11 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -137,17 +137,24 @@
 // service worker
 
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceWorker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+
+    
+    // Ensure that the browser supports the service worker API
+if (navigator.serviceWorker) {
+// Start registration process on every page load
+window.addEventListener('load', () => {
+	navigator.serviceWorker
+		// The register function takes as argument
+		// the file path to the worker's file
+		.register('/serviceWorker.js')
+		// Gives us registration object
+		.then(reg => console.log('Service Worker Registered'))
+		.catch(swErr => console.log(
+				`Service Worker Installation Error: ${swErr}}`));
+	});
 }
+
+    
 
 </script>
 
