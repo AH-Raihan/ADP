841bfd28138f992633c29bef8e85ce22a2484ed6
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-15 21:12:44 +0600

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -16,7 +16,7 @@
     <link rel="icon" href="../images/logo.png">
     <title>Alor Dishari Admin</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="manifest" href="<?php echo $host ?>/manifest.json">
+    <link rel="manifest" href="/admin/manifest.json">
     <link rel="stylesheet" type="text/css" href="css/reset.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="css/text.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="css/grid.css" media="screen" />
