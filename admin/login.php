21af2487eeb9cc3a2e435b31131395c83bfb71c8
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-15 21:36:27 +0600

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -12,23 +12,22 @@
 <div class="container">
 	<section id="row">
 		<form action="login_core.php" class="col-sm-6 mx-auto card bg-seconday" method="post">
-			<h1>Admin Login</h1>
+			<h1 class="text-center" >Admin Login</h1>
 			<div>
-				<input type="text" placeholder="User Name" required="" name="emailaddr"/>
+				<input type="text" class="form-control" placeholder="User Name" required="" name="emailaddr"/>
 			</div>
 			<div>
-				<input type="password" placeholder="Password" required="" name="pwd"/>
+				<input type="password" class="form-control my-3" placeholder="Password" required="" name="pwd"/>
 			</div>
 			<?php 
           if (isset($_REQUEST["wrongUsrPwd"])) { echo '<div style="color: red;font-weight:700;margin:10px;text-align:center;">Wrong User Or Password!</div>';} ?>
 			
 			<div>
-				<input type="submit" value="Log in" />
+				<input type="submit" class="btn btn-success btn-block"  value="Log in" />
 			</div>
-		</form><!-- form -->
-		<div class="button">
-			<a href="#">Alor Dishari Publications</a>
-		</div><!-- button -->
+<a href="../">Alor Dishari Publications</a>
+		</form>
+		
 	</section><!-- content -->
 </div><!-- container -->
 </body>
