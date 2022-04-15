60008ed2bbcfd42956ba87352ce087e5c8e2a59d
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-15 09:31:23 +0600

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -52,7 +52,7 @@ self.addEventListener("fetch", event => {
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function(cacheName) {
       return Promise.all(
         cacheNames.filter(function(cacheName) {
           // Return true if you want to remove this cache,
