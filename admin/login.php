17bd6ebc0dde8523494c05a30b0032a9a969cd87
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-15 09:36:06 +0600

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -6,6 +6,7 @@
 <meta charset="utf-8">
 <title>Login</title>
     <link rel="stylesheet" type="text/css" href="css/stylelogin.css" media="screen" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
 </head>
 <body>
 <div class="container">
