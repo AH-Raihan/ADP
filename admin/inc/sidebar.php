9d4117b0a6276e6d7ae0f74ef0c7bcf88a401142
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-16 14:37:39 +0600

    Update sidebar.php

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -5,10 +5,11 @@
                         <li class="nav-devider mt-0" style="margin-bottom: 5px"></li>
                         <li> <a href="index.php" ><span> <i class="fas fa-home"></i> </span><span class="hide-menu">Dashboard</span></a></li>
                         <li> <a href="orders.php" ><span> <i class="fa fa-cubes"></i> </span><span class="hide-menu">Orders</span></a></li>
-						<li> <a href="productlist.php" ><span> <i class="fa fa-book"></i> </span><span class="hide-menu">Products List</span></a></li>
-						<li> <a href="newslist.php" ><span> <i class="fas fa-newspaper"></i> </span><span class="hide-menu">News List</span></a></li>
-						<li> <a href="sliderlist.php" ><span> <i class="fas fa-images"></i> </span><span class="hide-menu">Slider List</span></a></li>
-					 <li> <a href="inbox.php" ><span> <i class="fas fa-envelope"></i> </span><span class="hide-menu">Inbox</span></a></li>
+			<li> <a href="productlist.php" ><span> <i class="fa fa-book"></i> </span><span class="hide-menu">Products List</span></a></li>
+			<li> <a href="newslist.php" ><span> <i class="fas fa-newspaper"></i> </span><span class="hide-menu">News List</span></a></li>
+			<li> <a href="sliderlist.php" ><span> <i class="fas fa-images"></i> </span><span class="hide-menu">Slider List</span></a></li>
+			<li> <a href="inbox.php" ><span> <i class="fas fa-envelope"></i> </span><span class="hide-menu">Inbox</span></a></li>
+			<li> <a href="changepassword.php" ><span> <i class="fas fa-key"></i> </span><span class="hide-menu">Inbox</span></a></li>
 				
                     </ul>
                 </nav>
