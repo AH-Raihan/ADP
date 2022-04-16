05ac89d4bded3086b3fe9799f741d1f447f410e1
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-16 14:08:09 +0600

    Update header.php

--- header.php
+++ header.php
@@ -86,7 +86,7 @@ global $user_id;
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" />
 <script>
 if (!navigator.onLine) {
-  window.location.pathname="./offline.html";
+  window.location.pathname="./index.html";
 }
 </script>
 
