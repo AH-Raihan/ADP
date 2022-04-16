bd1074045008aa9fd0ed6410591ea1cfadd4252f
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-16 11:08:07 +0600

    Update g_config.php

--- g_config.php
+++ g_config.php
@@ -1,8 +1,8 @@
 <?php
 require_once("google-api/vendor/autoload.php");
 $gClient = new Google_Client();
-$gClient->setClientId("370160254615-3rlctd9r9314jb55oehl2i1l3lp6k4l6.apps.googleusercontent.com");
-$gClient->setClientSecret("jjxJzUMODApQ3mZT6YNRSwZY");
+$gClient->setClientId("754003558572-08tg25lc1emvg3ti4a73lb18hoqgml3q.apps.googleusercontent.com");
+$gClient->setClientSecret("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");
 $gClient->setApplicationName("Alor Dishari Publications");
 $gClient->setRedirectUri("http://alordishari.herokuapp.com/controller.php");
 $gClient->addScope("https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/plus.me");
