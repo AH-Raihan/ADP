e4dc6c9abf956a3b45b1cd9d45afa20b79394192
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2022-04-16 14:39:13 +0600

    Update sidebar.php

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -9,7 +9,7 @@
 			<li> <a href="newslist.php" ><span> <i class="fas fa-newspaper"></i> </span><span class="hide-menu">News List</span></a></li>
 			<li> <a href="sliderlist.php" ><span> <i class="fas fa-images"></i> </span><span class="hide-menu">Slider List</span></a></li>
 			<li> <a href="inbox.php" ><span> <i class="fas fa-envelope"></i> </span><span class="hide-menu">Inbox</span></a></li>
-			<li> <a href="changepassword.php" ><span> <i class="fas fa-key"></i> </span><span class="hide-menu">Inbox</span></a></li>
+			<li> <a href="changepassword.php" ><span> <i class="fas fa-key"></i> </span><span class="hide-menu">Change Password</span></a></li>
 				
                     </ul>
                 </nav>
